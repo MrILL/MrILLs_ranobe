a6d5be07098c77f8c3dabd9a816bdd9ae43cc010
MrILL <ilua2403@ukr.net>
2021-06-03 17:12:03 +0300

    feat(ranobeDomains): init CRUD

--- src/repositories/ranobeDomains.js
+++ src/repositories/ranobeDomains.js
@@ -6,20 +6,46 @@ class RanobeDomainsRepo {
   async create({ ranobeId, domain, url }) {
     const insertQuery =
       'INSERT INTO ranobeDomains (ranobeId, domain, source) \
-    VALUES ($1, $2, $3) RETURNING *';
+      VALUES ($1, $2, $3) RETURNING *';
     const values = [ranobeId, domain, url];
     const { rows } = await this.db.query(insertQuery, values);
 
     return rows[0];
   }
 
-  async getOneByDomain({ ranobeId, domain }) {
+  async get({ ranobeId }) {
+    const selectQuery = 'SELECT * FROM ranobeDomains WHERE ranobeId=$1';
+    const values = [ranobeId];
+    const { rows } = await this.db.query(selectQuery, values);
+    return rows;
+  }
+
+  async getOne({ ranobeId, domain }) {
     const selectQuery =
       'SELECT * FROM ranobeDomains WHERE ranobeId=$1 AND domain=$2';
     const values = [ranobeId, domain];
     const { rows } = await this.db.query(selectQuery, values);
 
     return rows[0];
+  }
+
+  async update({ ranobeId, domain, url }) {
+    const updateQuery =
+      'UPDATE ranobeDomains \
+      SET source=$3 \
+      WHERE ranobeId=$1 AND domain=$2 \
+      RETURNING *';
+    const values = [ranobeId, domain, url];
+    const { rows } = await this.db.query(updateQuery, values);
+
+    return rows[0];
+  }
+
+  async delete({ ranobeId, domain }) {
+    const deleteQuery =
+      'DELETE FROM ranobeDomains WHERE ranobeId=$1 AND domain=$2';
+    const values = [ranobeId, domain];
+    await this.db.query(deleteQuery, values);
   }
 }
 
