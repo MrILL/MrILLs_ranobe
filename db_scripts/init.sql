193891c2168271482c66dd49d7751df952a96536
MrILL <ilua2403@ukr.net>
2021-06-21 15:36:52 +0300

    feat(db): cascade deleting

--- db_scripts/init.sql
+++ db_scripts/init.sql
@@ -3,24 +3,26 @@ CREATE TABLE IF NOT EXISTS ranobes (
     title VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS ranobeDomains (
-    id VARCHAR(7) PRIMARY KEY,
-    ranobeId VARCHAR(7) REFERENCES ranobes NOT NULL,
-    domain VARCHAR(255) NOT NULL,
-    source VARCHAR(2048) NOT NULL
-);
 --   TODO:
 --   //genre
 --   //finished --//get status from source
 --   //total chapters
 --   //date of last update?
+CREATE TABLE IF NOT EXISTS ranobeDomains (
+    id VARCHAR(7) PRIMARY KEY,
+    ranobeId VARCHAR(7) NOT NULL,
+    domain VARCHAR(255) NOT NULL,
+    source VARCHAR(2048) NOT NULL
+);
+ALTER TABLE ranobeDomains ADD FOREIGN KEY (ranobeId) REFERENCES ranobes ON DELETE CASCADE;
 
+-- TODO deside about changing type of 'nomer' from INT to VARCHAR
 CREATE TABLE IF NOT EXISTS chapters (
     id VARCHAR(7) PRIMARY KEY,
-    ranobeDomainId VARCHAR(7) REFERENCES ranobeDomains,
+    ranobeDomainId VARCHAR(7) NOT NULL,
     nomer INT NOT NULL,
     source VARCHAR(2048) NOT NULL,
     title VARCHAR(255),
     body TEXT
 );
--- TODO deside about changing type of 'nomer' from INT to VARCHAR
+ALTER TABLE chapters ADD FOREIGN KEY (ranobeDomainId) REFERENCES ranobeDomains ON DELETE CASCADE;
